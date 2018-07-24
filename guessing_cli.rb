ee3aa89d6a70baf567e9c0c430b94a250277f5e5
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-24 14:39:00 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,7 @@
-# Code your solution here!
+def run_guessing_game
+  puts "Guess a number, type exit to stop"
+  command = ""
+  until command.downcase == "exit"
+    command = gets.chomp 
+    if command = 
+  
