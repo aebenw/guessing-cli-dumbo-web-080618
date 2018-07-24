2461a15aec41083ec6553025e2c4bc46b67fa33e
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-24 14:45:23 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,11 +2,24 @@ def exit_gen
   puts "Goodbye!"
 end
 
+
+
 def run_guessing_game
   puts "Guess a number between 1 and 6, type exit to stop"
   command = ""
-  number = 
+  number = rand(1..6)
   until command.downcase == "exit"
     command = gets.chomp 
-    if command = 
+    if command.to_i = number
+      puts "You guessed the correct number!"
+      command = gets.chomp
+    elsif command != number
+      puts "The computer guessed 5."
+      command = gets.chomp
+    elsif command.downcase == exit
+      exit_gen
+    end 
+  end
+end
+    
   
