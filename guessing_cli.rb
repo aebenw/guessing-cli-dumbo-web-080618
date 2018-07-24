0e7a3174c1f3f81aff356e75444ec16be2bc894c
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-24 14:41:23 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,5 +1,9 @@
+def exit_gen
+  puts "Goodbye!"
+end
+
 def run_guessing_game
-  puts "Guess a number, type exit to stop"
+  puts "Guess a number between 1 and 6, type exit to stop"
   command = ""
   number = 
   until command.downcase == "exit"
